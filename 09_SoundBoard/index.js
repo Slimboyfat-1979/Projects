17fd394ba3ef499ac3a83ee12194e52409206cac
Dave <slimboyfat1979@gmail.com>
2024-04-20 14:47:29 +1200

    Added new files and completed project

---
+++ 09_SoundBoard/index.js
@@ -0,0 +1,23 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    btn.style.textTransform  = 'Capitalize';
+    document.getElementById('buttons').appendChild(btn);
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+}) 
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+        console.log(song);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
